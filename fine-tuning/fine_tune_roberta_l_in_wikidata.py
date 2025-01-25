804cd0e09fc9ac52a860fe6cc62155e10c1d5d65
ignacioct <ignaciotalaveracepeda@gmail.com>
2025-01-25 16:40:45 +0100

    File for fine tuning wikidata created

---
+++ fine-tuning/fine_tune_roberta_l_in_wikidata.py
@@ -0,0 +1,152 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
+import wandb
+from datasets import ClassLabel, Dataset, DatasetDict, Features, Value
+from roberta_for_entity_pair_classification import RobertaForEntityPairClassification
+from sklearn.metrics import precision_recall_fscore_support
+from tacred_slots import TACRED_SLOTS
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    Trainer,
+    TrainingArguments,
+)
+
+# Log in to W&B using API key
+wandb.login(key="7bd265df21100baa9767bb9f69108bc417db4b4a")
+
+# Load tokenizer and model configuration
+model_name = "FacebookAI/roberta-large"
+tokenizer = AutoTokenizer.from_pretrained(
+    model_name, use_fast=False, add_prefix_space=True
+)
+tokenizer.add_tokens(["[E1]", "[/E1]", "[E2]", "[/E2]"])
+head_token_id, tail_token_id = tokenizer.convert_tokens_to_ids(["[E1]", "[E2]"])
+
+config = AutoConfig.from_pretrained(model_name, num_labels=len(TACRED_SLOTS))
+model = RobertaForEntityPairClassification.from_pretrained(model_name, config=config)
+model.resize_token_embeddings(len(tokenizer))
+
+model.config.label2id = {label: i for i, label in enumerate(TACRED_SLOTS)}
+model.config.id2label = {i: label for i, label in enumerate(TACRED_SLOTS)}
+model.config.head_token = "[E1]"
+model.config.head_token_id = head_token_id
+model.config.tail_token = "[E2]"
+model.config.tail_token_id = tail_token_id
+
+data_collator = DataCollatorWithPadding(tokenizer, pad_to_multiple_of=8)
+
+
+def tokenize_function(examples):
+    result = tokenizer(examples["text"], padding=False, truncation=True)
+    result["head_pos"] = [
+        next((i for i, token in enumerate(tokens) if token == head_token_id), -1)
+        for tokens in result["input_ids"]
+    ]
+    result["tail_pos"] = [
+        next((i for i, token in enumerate(tokens) if token == tail_token_id), -1)
+        for tokens in result["input_ids"]
+    ]
+    result["label"] = examples["label"]
+    return result
+
+
+def compute_metrics(p):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=-1)
+    positive_labels = [
+        i for i, label in enumerate(TACRED_SLOTS) if label != "no_relation"
+    ]
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        labels, predictions, labels=positive_labels, average="micro"
+    )
+    return {"precision": precision, "recall": recall, "f1": f1}
+
+
+def convert_rows_to_training_dict(dataset: pd.DataFrame) -> List:
+    output = []
+    for _, row in dataset.iterrows():
+        if row["relation"] not in TACRED_SLOTS:
+            continue
+        row["token"].insert(row["subj_start"], "[E1]")
+        row["token"].insert(row["subj_end"] + 2, "[/E1]")
+        row["token"].insert(row["obj_start"], "[E2]")
+        row["token"].insert(row["obj_end"] + 2, "[/E2]")
+        text = " ".join(row["token"])
+        output.append(
+            {"text": text, "head": "T1", "tail": "T2", "label": row["relation"]}
+        )
+    return output
+
+
+def preprocess_data(
+    train_data: pd.DataFrame, test_data: pd.DataFrame, valid_data: pd.DataFrame
+) -> DatasetDict:
+    train_output = convert_rows_to_training_dict(train_data)
+    test_output = convert_rows_to_training_dict(test_data)
+    valid_output = convert_rows_to_training_dict(valid_data)
+    features = Features(
+        {
+            "text": Value(dtype="string"),
+            "head": Value(dtype="string"),
+            "tail": Value(dtype="string"),
+            "label": ClassLabel(names=TACRED_SLOTS),
+        }
+    )
+    return DatasetDict(
+        {
+            "train": Dataset.from_list(train_output, features=features),
+            "test": Dataset.from_list(test_output, features=features),
+            "valid": Dataset.from_list(valid_output, features=features),
+        }
+    )
+
+
+def train():
+    with wandb.init(project="TACRED-RoBERTa-Large"):
+        train_data = pd.read_json("../data/tacred/train.json")
+        test_data = pd.read_json("../data/tacred/test.json")
+        valid_data = pd.read_json("../data/tacred/dev.json")
+
+        dataset = preprocess_data(train_data, test_data, valid_data)
+        tokenized_dataset = dataset.map(tokenize_function, batched=True)
+
+        train_args = TrainingArguments(
+            do_train=True,
+            do_eval=True,
+            evaluation_strategy="epoch",
+            save_strategy="epoch",
+            per_device_train_batch_size=32,
+            per_device_eval_batch_size=32,
+            learning_rate=0.00003,
+            weight_decay=0.01,
+            num_train_epochs=5,
+            fp16=True,
+            save_total_limit=1,
+            load_best_model_at_end=True,
+            output_dir="tmp/",
+        )
+
+        trainer = Trainer(
+            model=model,
+            args=train_args,
+            train_dataset=tokenized_dataset["train"],
+            eval_dataset=tokenized_dataset["valid"],
+            tokenizer=tokenizer,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+        )
+        trainer.train()
+
+        trainer.save_model("fine-tuned-roberta-large-tacred")
+
+
+def main():
+    train()
+
+
+if __name__ == "__main__":
+    main()
